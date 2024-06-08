e7380e0d5e775a5f23fba7459da7be92214659b9
czwlines <czwlines@gmail.com>
2024-06-08 22:14:37 +0800

    lines

--- infer.sh
+++ infer.sh
@@ -1,13 +1,15 @@
+data_path=/path-to-data
+compressor_path=/path-to-compressor
+lm_model_path=/path-to-longchat-13b
+from_checkpoint=/path-to-checkpoint
+save_path=/path-to-save-generation
+
 batch_size=4
 lm_model_name=longchat
 compressor_hidden_size=4096
 lm_model_hidden_size=5120
-data_path=/path-to-data
-compressor_path=/path-to-compressor
-lm_model_path=/path-to-longchat-13b
 num_compressor_layers=4
 num_compressor_encoder_layers=2
-from_checkpoint=/path-to-checkpoint
 benchmark_metric=accuracy
 num_eval_documents=4
 pw_window_sizes=(2 4 6 8)
@@ -15,7 +17,6 @@ pw_window_sizes_str=$(printf "_%s" "${pw_window_sizes[@]}")
 pw_window_sizes_str=${pw_window_sizes_str:1}
 
 echo num_eval_documents=${num_eval_documents}-pw=${pw_window_sizes_str}
-save_path=/path-to-save-generation
 mkdir -p $save_path
 
 accelerate launch --config_file config/bf16.yaml \
