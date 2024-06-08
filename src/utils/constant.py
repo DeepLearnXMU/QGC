075c49fa78bec43dd54637003705124efd459765
Zhiwei <59118395+czwlines@users.noreply.github.com>
2024-06-08 22:36:21 +0800

    Update constant.py

--- src/utils/constant.py
+++ src/utils/constant.py
@@ -6,6 +6,6 @@ SCHEDULER_NAME = "scheduler.pt"
 TRAINING_ARGS_NAME = "training_args.bin"
 TRAINER_STATE_NAME = "trainer_state.json"
 
-FFN_WEIGHTS_NAME = "align_ffn.pytorch_model.bin"
+FFN_WEIGHTS_NAME = "ffn.pytorch_model.bin"
 COMPRESSOR_WEIGHTS_NAME = "compressor.pytorch_model.bin"
 POOLING_WEIGHTS_NAME = "pooling_layer.pytorch_model.bin"
