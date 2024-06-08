866e0072fcac20e11b1a8303f25c4edbd43e1abd
Zhiwei <59118395+czwlines@users.noreply.github.com>
2024-06-08 22:13:31 +0800

    Update infer.sh

--- infer.sh
+++ infer.sh
@@ -2,12 +2,12 @@ batch_size=4
 lm_model_name=longchat
 compressor_hidden_size=4096
 lm_model_hidden_size=5120
-data_path=data/nq/test.sorted-longllmlingua.jsonl
-compressor_path=/mnt/bn/multilingual-translation-ckpt/caozhiwei/workspace/context-compress/pretrained-models/4l_llama
-lm_model_path=/mnt/bn/multilingual-translation-ckpt/caozhiwei/models/longchat-13b-16k
+data_path=/path-to-data
+compressor_path=/path-to-compressor
+lm_model_path=/path-to-longchat-13b
 num_compressor_layers=4
 num_compressor_encoder_layers=2
-from_checkpoint=save/nq-gf-rp-best
+from_checkpoint=/path-to-checkpoint
 benchmark_metric=accuracy
 num_eval_documents=4
 pw_window_sizes=(2 4 6 8)
@@ -15,10 +15,7 @@ pw_window_sizes_str=$(printf "_%s" "${pw_window_sizes[@]}")
 pw_window_sizes_str=${pw_window_sizes_str:1}
 
 echo num_eval_documents=${num_eval_documents}-pw=${pw_window_sizes_str}
-
-save_dir=generations/nq-gf-rp-best
-mkdir -p $save_dir
-save_path=${save_dir}/longllmlingua
+save_path=/path-to-save-generation
 mkdir -p $save_path
 
 accelerate launch --config_file config/bf16.yaml \
